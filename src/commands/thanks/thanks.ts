6dd3c1f303806c070682b2ae8560c47bf4cb592a
filipMaM <filip.mamcarczyk@gmail.com>
2022-07-20 21:56:01 +0200

    Seperate update functions

--- src/commands/thanks/thanks.ts
+++ src/commands/thanks/thanks.ts
@@ -52,30 +52,8 @@ export const handler = async (
         return;
     }
 
-    findDiscordUser(author).then(async (user) => {
-        DiscordUser.updateOne(
-            {
-                discordId: author.id,
-            },
-            {
-                thanksGiven: user?.thanksGiven ? user?.thanksGiven + 1 : 1,
-                lastGiven: createdTimestamp,
-            }
-        ).then();
-    });
-
-    findDiscordUser(target).then(async (user) => {
-        DiscordUser.updateOne(
-            {
-                discordId: target.id,
-            },
-            {
-                thanksReceived: user?.thanksReceived
-                    ? user?.thanksReceived + 1
-                    : 1,
-            }
-        ).then();
-    });
+    await updateUserAndIncrementGiven(author, createdTimestamp);
+    await updateUserAndIncrementReceived(target);
 
     Thanks.create({
         author: author.id,
@@ -102,6 +80,41 @@ const findDiscordUser = async (user?: User) => {
     }
 
     return record as IDiscordUser;
+};
+
+const updateUserAndIncrementGiven = async (user: User, lastGiven: number) => {
+    const exitistingUser = await DiscordUser.updateOne(
+        {
+            discordId: user.id,
+        },
+        { $inc: { thanksGiven: 1 }, lastGiven }
+    );
+
+    if (exitistingUser.matchedCount === 0) {
+        await DiscordUser.create({
+            discordId: user.id,
+            username: user.username,
+            thanksGiven: 1,
+            lastGiven,
+        });
+    }
+};
+
+const updateUserAndIncrementReceived = async (user: User) => {
+    const exitistingUser = await DiscordUser.updateOne(
+        {
+            discordId: user.id,
+        },
+        { $inc: { thanksReceived: 1 } }
+    );
+
+    if (exitistingUser.matchedCount === 0) {
+        await DiscordUser.create({
+            discordId: user.id,
+            username: user.username,
+            thanksReceived: 1,
+        });
+    }
 };
 
 export default { run, handler };
